70224465acc5a0261060160c6857dda83c5583ab
Curt Tyler <r.curt.tyler@gmail.com>
2017-07-12 17:32:43 -0700

    refactor: don't care aboout settings cookies for each request

--- src/client/request.js
+++ src/client/request.js
@@ -1,40 +1,25 @@
 import request from 'superagent';
 
-function getCookie(name) {
-	var value = '; ' + document.cookie;
-	var parts = value.split('; ' + name + '=');
-	if (parts.length == 2) {
-		return parts.pop().split(';').shift();
-	}
-}
-
 export default {
 	get: uri => {
-		const token = getCookie('csrf_token');
-		return token ? request.get(uri).set('X-Csrf-Token', token) : request.get(uri);
+		return request.get(uri);
 	},
 	post: uri => {
-		const token = getCookie('csrf_token');
-		return token ? request.post(uri).set('X-Csrf-Token', token) : request.post(uri);
+		return request.post(uri);
 	},
 	put: uri => {
-		const token = getCookie('csrf_token');
-		return token ? request.put(uri).set('X-Csrf-Token', token) : request.put(uri);
+		return request.put(uri);
 	},
 	head: uri => {
-		const token = getCookie('csrf_token');
-		return token ? request.head(uri).set('X-Csrf-Token', token) : request.head(uri);
+		return request.head(uri);
 	},
 	del: uri => {
-		const token = getCookie('csrf_token');
-		return token ? request.del(uri).set('X-Csrf-Token', token) : request.del(uri);
+		return request.del(uri);
 	},
 	trace: uri => {
-		const token = getCookie('csrf_token');
-		return token ? request.trace(uri).set('X-Csrf-Token', token) : request.trace(uri);
+		return request.trace(uri);
 	},
 	connect: uri => {
-		const token = getCookie('csrf_token');
-		return token ? request.connect(uri).set('X-Csrf-Token', token) : request.connect(uri);
+		return request.connect(uri);
 	},
 };
